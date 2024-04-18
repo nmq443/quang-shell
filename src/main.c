f9349d1afb68fc1dded73d55cae8caf94021d166
nmq443 <21020564@vnu.edu.vn>
2024-04-18 12:36:12 +0700

    add path_handle

--- src/main.c
+++ src/main.c
@@ -23,7 +23,70 @@ char **qsh_split_line(char *line);
 // execute
 void qsh_execute();
 
-int x = 0;
+
+// built-in commands
+// cd, exit, help 
+int qsh_cd(char** args);
+int qsh_help(char** args);
+int qsh_exit(char** args);
+char* builtin_cmds[] = 
+{
+	"cd", 
+	"help",
+	"exit"
+};
+
+int (*builtin_func[]) (char**) = 
+{
+	&qsh_cd,
+	&qsh_help,
+	&qsh_exit
+};
+
+int qsh_num_builtins()
+{
+	return sizeof(builtin_cmds) / sizeof(char*);
+}
+
+// path handler
+struct node {
+	char *dir;
+	struct node* next;
+};
+
+typedef struct node node;
+
+node *create_path_list(char *path)
+{
+	node *head = NULL;
+	node *tail = NULL;
+
+	// parse the path and split into dirs
+	char* dir = strtok(path, ":");
+	
+	while (dir != NULL)
+	{
+		// new node for dir
+		node *new_node = (node*) malloc (sizeof(node));
+		new_node->dir = dir;
+		new_node->next = NULL;
+
+		// add the new node to the list
+		if (!head)
+		{
+			head = new_node;
+			tail = new_node;
+		} else 
+		{
+			tail->next = new_node;
+			tail = new_node;
+		}
+
+		dir = strtok(NULL, ":");
+	}
+
+	return head;
+}
 
 int main(int argc, char** argv)
 {
@@ -33,7 +96,7 @@ int main(int argc, char** argv)
 
 void qsh_loop()
 {
-	//do {
+	do {
 		if (isatty(STDIN_FILENO)) // interactive mode
 		{
 			qsh_prompt();			
@@ -42,7 +105,7 @@ void qsh_loop()
 		{
 			printf("Non-interactive mode");
 		}
-	//} ;
+	} while(1);
 }
 
 char* qsh_read_line()
@@ -150,4 +213,40 @@ char **qsh_split_line(char *line)
 void qsh_execute()
 {
 
+}
+
+int qsh_cd(char **args)
+{
+	if (!args[1])
+	{
+		fprintf(stderr, "qsh: expected argument to \"cd\"\n");
+		exit(EXIT_FAILURE);
+	} else 
+	{
+		if (chdir(args[1]))
+		{
+			perror("qsh");
+		}
+	}
+	return 1;
+}
+
+int qsh_help(char **args)
+{
+	int i;
+	printf("My shell\n");
+	printf("Built-in commands: \n");
+
+	for (int i = 0; i < qsh_num_builtins(); i++)
+	{
+		printf(" %s\n", builtin_cmds[i]);
+	}
+
+	printf("Use the man command for information on other programs\n");
+	return 1;
+}
+
+int qsh_exit(char **args)
+{
+	return 0;
 }
